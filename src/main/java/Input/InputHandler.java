d2615072f658b5b3082aba96956968700637bc62
JVSRamos <joaoramosj@usp.br>
2022-05-13 15:59:00 +0200

    feat: Start creating input handler

---
+++ src/main/java/Input/InputHandler.java
@@ -0,0 +1,10 @@
+package Input;
+
+public interface InputHandler {
+
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+
+}
---
+++ src/main/java/Input/KeyBoardHandler.java
@@ -0,0 +1,40 @@
+package Input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardHandler implements InputHandler, KeyListener {
+    @Override
+    public void moveLeft() {
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
