faca371c26db5afc19b8df1756f7b0f5d172dd24
JVSRamos <joaoramosj@usp.br>
2022-05-16 23:15:54 +0200

    feat: implement GameComponentGraphicView

--- src/main/java/Views/graphics/GameComponentGraphicView.java
+++ src/main/java/Views/graphics/GameComponentGraphicView.java
@@ -1,13 +1,37 @@
 package Views.graphics;
 
+import Views.ComponentView;
+
 import javax.swing.*;
+import java.awt.*;
 
-public abstract class GameComponentGraphicView extends JComponent {
+public abstract class GameComponentGraphicView extends JComponent implements ComponentView {
 
-    protected JComponent icon;
+    JLabel iconContainer;
+    final int DEFAULT_TILE_SIZE = 32;
 
-    public GameComponentGraphicView(JComponent icon) {
-        this.icon = icon;
+    public GameComponentGraphicView() {
+        iconContainer = new JLabel();
+        iconContainer.setSize(new Dimension(DEFAULT_TILE_SIZE, DEFAULT_TILE_SIZE));
+    }
+    public GameComponentGraphicView(Icon icon) {
+        iconContainer = new JLabel();
+        iconContainer.setIcon(icon);
+        iconContainer.setSize(new Dimension(icon.getIconWidth(), icon.getIconHeight()));
+    }
+
+    @Override
+    public void showComponent() {}
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        super.paintComponent(g2);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(this.iconContainer.getWidth(), this.iconContainer.getHeight());
     }
 
 }
