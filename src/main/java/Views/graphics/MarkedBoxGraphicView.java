c340f804498c09bc93a5613b6dfec10265058b37
JVSRamos <joaoramosj@usp.br>
2022-05-22 13:51:18 +0200

    feat: Add markedcrate view

---
+++ src/main/java/Views/graphics/MarkedBoxGraphicView.java
@@ -0,0 +1,10 @@
+package Views.graphics;
+
+import javax.swing.*;
+
+public class MarkedBoxGraphicView extends GameModelGraphicView {
+
+    public MarkedBoxGraphicView() {
+        super(new ImageIcon("src/main/resources/sokoban_icons/cratemarked.png"));
+    }
+}
