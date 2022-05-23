b35e309c82a96e76f7cf13b6c7aed6d05367b161
JVSRamos <joaoramosj@usp.br>
2022-05-23 21:50:47 +0200

    refact: Remove unused methods

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -1,11 +1,8 @@
 import Controllers.BoardController;
-import WinHandler.SokobanWinHandler;
 
 public class GameState {
 
-    private SokobanWinHandler winHandler;
     private BoardController boardController;
-
     public GameState(BoardController boardController) {
         this.boardController = boardController;
     }
